bccc65d809ac867b8d8d80c4ecf7aaaea85b1b57
FeliciaDixon-WA <fddixonwa@gmail.com>
2020-05-29 14:56:27 -0700

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-uploads-felicia"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://43b8svpcx4.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_ZZFmKQI37",
+      APP_CLIENT_ID: "3i6g9ujuamh2m7gg6np3ci6145",
+      IDENTITY_POOL_ID: "us-east-1:1fde2c5b-4e92-4b63-816c-ae4a9309d0b5"
+    }
+  };
